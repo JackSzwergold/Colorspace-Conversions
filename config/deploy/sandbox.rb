a6c216577d3f8dc2499ae5887941e809ca96dfdf
Jack Szwergold <JackSzwergold@gmail.com>
2016-04-19 23:50:46 -0400

    Even more adjustments to move stuff back to Preworn.com.

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -7,6 +7,9 @@ server 'sandbox.local', user: ENV["CAP_USER"] || 'vagrant', roles: %w{app db web
 # The directory on the server into which the actual source code will deployed.
 set :web_builds, "#{deploy_to}/builds"
 
+# The path where projects get deployed.
+set :projects_path, "projects"
+
 # The directory on the server that stores content related data.
 set :content_data_path, "#{deploy_to}/content"
 
@@ -26,14 +29,14 @@ namespace :deploy do
   task :create_symlink do
     on roles(:app) do
 
-      info "If there is no directory & no symbolic link to 'site/art' then create a directory named 'site/art'."
-      execute "cd #{fetch(:live_root)} && if [ ! -d site/art ]; then if [ ! -h site/art ]; then mkdir ./site/art; fi; fi"
+        # info "If there is no directory & no symbolic link to 'site/#{fetch(:projects_path)}' then create a directory named 'site/#{fetch(:projects_path)}'."
+        execute "cd #{fetch(:live_root)} && if [ ! -d site/#{fetch(:projects_path)} ]; then if [ ! -h site/#{fetch(:projects_path)} ]; then mkdir ./site/#{fetch(:projects_path)}; fi; fi"
 
-      info "If there is a symbolic link to 'site/art' then create a symbolic link called 'site/art'."
-      execute "cd #{fetch(:live_root)} && if [ ! -h site/art ]; then if [ ! -d site/art ]; then ln -sf #{current_path} ./site/art; fi; fi"
+        # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}' then create a symbolic link called 'site/#{fetch(:projects_path)}'."
+        execute "cd #{fetch(:live_root)} && if [ ! -h site/#{fetch(:projects_path)} ]; then if [ ! -d site/#{fetch(:projects_path)} ]; then ln -sf #{current_path} ./site/#{fetch(:projects_path)}; fi; fi"
 
-      info "If there is a symbolic link to 'site/art/#{fetch(:short_name)}', delete it. Irregardless, create a new symbolic link to 'site/art/#{fetch(:short_name)}'."
-      execute "cd #{fetch(:live_root)} && if [ -h site/art/#{fetch(:short_name)} ]; then rm site/art/#{fetch(:short_name)}; fi && ln -sf #{current_path} ./site/art/#{fetch(:short_name)}"
+        # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}', delete it. Irregardless, create a new symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}'."
+        execute "cd #{fetch(:live_root)} && if [ -h site/#{fetch(:projects_path)}/#{fetch(:short_name)} ]; then rm site/#{fetch(:projects_path)}/#{fetch(:short_name)}; fi && ln -sf #{current_path} ./site/#{fetch(:projects_path)}/#{fetch(:short_name)}"
 
     end
   end
