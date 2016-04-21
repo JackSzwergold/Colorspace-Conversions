a8e69871aad9f3e6596ccb2039a22a06ce5ecbfe
Jack Szwergold <JackSzwergold@gmail.com>
2016-04-20 22:35:24 -0400

    Consolidating some stuff into the main 'deploy.rb' file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,7 @@ set :application, 'colorspace_conversions'
 set :short_name, 'colorspace'
 set :repo_url, 'git@github.com:JackSzwergold/Colorspace-Conversions.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
+# Set the 'deploy_to' directory.
 set :deploy_to, '/var/www'
 
 # Default value for :scm is :git
@@ -21,19 +18,32 @@ set :format, :pretty
 set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, false
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# set :linked_dirs, fetch(:linked_dirs, []).push('cache')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
+
+# Disable warnings about the absence of the styleseheets, javscripts & images directories.
+set :normalize_asset_timestamps, false
+
+# The directory on the server into which the actual source code will deployed.
+set :web_builds, "#{deploy_to}/builds"
+
+# The directory on the server that stores content related data.
+set :content_data_path, "#{deploy_to}/content"
+
+# The path where projects get deployed.
+set :projects_path, "projects"
 
 namespace :deploy do
 
@@ -46,4 +56,23 @@ namespace :deploy do
     end
   end
 
+  # Create the 'create_symlink' task to create symbolic links and other related items.
+  desc "Set the symbolic links."
+  task :create_symlink do
+    on roles(:app) do
+
+      # info "If there is no directory & no symbolic link to 'site/#{fetch(:projects_path)}' then create a directory named 'site/#{fetch(:projects_path)}'."
+      execute "cd #{fetch(:live_root)} && if [ ! -d site/#{fetch(:projects_path)} ]; then if [ ! -h site/#{fetch(:projects_path)} ]; then mkdir ./site/#{fetch(:projects_path)}; fi; fi"
+
+      # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}' then create a symbolic link called 'site/#{fetch(:projects_path)}'."
+      execute "cd #{fetch(:live_root)} && if [ ! -h site/#{fetch(:projects_path)} ]; then if [ ! -d site/#{fetch(:projects_path)} ]; then ln -sf #{current_path} ./site/#{fetch(:projects_path)}; fi; fi"
+
+      # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}', delete it. Irregardless, create a new symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}'."
+      execute "cd #{fetch(:live_root)} && if [ -h site/#{fetch(:projects_path)}/#{fetch(:short_name)} ]; then rm site/#{fetch(:projects_path)}/#{fetch(:short_name)}; fi && ln -sf #{current_path} ./site/#{fetch(:projects_path)}/#{fetch(:short_name)}"
+
+    end
+  end
+
 end
+
+after "deploy:published", "deploy:create_symlink"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,44 +4,11 @@ set :branch, "develop"
 # The details of the destination server you will be deploying to.
 server 'prod0.preworn.com', user: ENV["CAP_USER"] || 'sysop', roles: %w{app db web}, my_property: :my_value
 
-# The directory on the server into which the actual source code will deployed.
-set :web_builds, "#{deploy_to}/builds"
-
-# The path where projects get deployed.
-set :projects_path, "projects"
-
-# The directory on the server that stores content related data.
-set :content_data_path, "#{deploy_to}/content"
+# Set the name for the deployment type.
+set :deployment_type, "staging"
 
 # The live, web root directory which the current version will be linked to.
 set :live_root, "#{deploy_to}/staging.preworn.com"
 
 # Set the 'deploy_to' directory for this task.
-set :deploy_to, "/var/www/builds/#{fetch(:application)}/staging"
-
-# Disable warnings about the absence of the styleseheets, javscripts & images directories.
-set :normalize_asset_timestamps, false
-
-# Create the 'create_symlink' task to create symbolic links and other related items.
-namespace :deploy do
-
-  desc "Set the symbolic links."
-  task :create_symlink do
-    on roles(:app) do
-
-        # info "If there is no directory & no symbolic link to 'site/#{fetch(:projects_path)}' then create a directory named 'site/#{fetch(:projects_path)}'."
-        execute "cd #{fetch(:live_root)} && if [ ! -d site/#{fetch(:projects_path)} ]; then if [ ! -h site/#{fetch(:projects_path)} ]; then mkdir ./site/#{fetch(:projects_path)}; fi; fi"
-
-        # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}' then create a symbolic link called 'site/#{fetch(:projects_path)}'."
-        execute "cd #{fetch(:live_root)} && if [ ! -h site/#{fetch(:projects_path)} ]; then if [ ! -d site/#{fetch(:projects_path)} ]; then ln -sf #{current_path} ./site/#{fetch(:projects_path)}; fi; fi"
-
-        # info "If there is a symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}', delete it. Irregardless, create a new symbolic link to 'site/#{fetch(:projects_path)}/#{fetch(:short_name)}'."
-        execute "cd #{fetch(:live_root)} && if [ -h site/#{fetch(:projects_path)}/#{fetch(:short_name)} ]; then rm site/#{fetch(:projects_path)}/#{fetch(:short_name)}; fi && ln -sf #{current_path} ./site/#{fetch(:projects_path)}/#{fetch(:short_name)}"
-
-    end
-  end
-
-end
-
-# after :deploy, "deploy:create_symlink"
-after "deploy:published", "deploy:create_symlink"
+set :deploy_to, "/var/www/builds/#{fetch(:application)}/#{fetch(:deployment_type)}"
