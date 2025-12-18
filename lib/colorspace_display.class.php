d342b4b66581ef200308b295bf4d76c82cae0ca8
Jack Szwergold <JackSzwergold@gmail.com>
2025-12-18 14:59:59 -0500

    More work.

--- lib/colorspace_display.class.php
+++ lib/colorspace_display.class.php
@@ -36,7 +36,7 @@ class Display extends Helpers {
   public $gray = NULL;
   public $gray_percentage = 0;
 
-  public $gray_text_cutoff = 32;
+  public $gray_text_cutoff = 36;
 
   public $hex = NULL;
   public $hex_inverted = NULL;
@@ -227,12 +227,12 @@ class Display extends Helpers {
     /************************************************************************************/
     // Set all of the different values.
     $ret = array();
-    $ret['hex'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex, '#'), $css_for_text, $this->hex);
-    $ret['hex_inverted'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex_inverted, '#'), $css_for_text, $this->hex_inverted);
-    $ret['hex_complimentary'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex_inverted, '#'), $css_for_text, $this->hex_inverted);
-    $ret['hex_gray'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex_gray, '#'), $css_for_text, $this->hex_gray);
-    $ret['hex_gray_inverted'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex_gray_inverted, '#'), $css_for_text, $this->hex_gray_inverted);
-    $ret['hex_gray_complimentary'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="%s">%s</a>', ltrim($this->hex_gray_inverted, '#'), $css_for_text, $this->hex_gray_inverted);
+    $ret['hex'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex, '#'), $css_for_text, $this->hex);
+    $ret['hex_inverted'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex_inverted, '#'), $css_for_text, $this->hex_inverted);
+    $ret['hex_complimentary'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex_inverted, '#'), $css_for_text, $this->hex_inverted);
+    $ret['hex_gray'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex_gray, '#'), $css_for_text, $this->hex_gray);
+    $ret['hex_gray_inverted'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex_gray_inverted, '#'), $css_for_text, $this->hex_gray_inverted);
+    $ret['hex_gray_complimentary'] = sprintf('<a href="' . BASE_URL . 'hex/%s" class="text-decoration-none %s">%s</a>', ltrim($this->hex_gray_inverted, '#'), $css_for_text, $this->hex_gray_inverted);
     $ret['rgb'] = sprintf('R = %s, G = %s, B = %s', $rgb_array['red'], $rgb_array['green'], $rgb_array['blue']);
     $ret['gray'] = sprintf('R = %s, G = %s, B = %s', $this->gray['red'], $this->gray['green'], $this->gray['blue']);
     $ret['gray_percentage'] = sprintf('%s', $this->gray_percentage . '%');
